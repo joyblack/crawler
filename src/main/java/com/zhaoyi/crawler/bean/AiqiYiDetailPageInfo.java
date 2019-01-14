a0890af32995234bd46ae97c21c89085cf14c6eb
joyblack <1016037677@qq.com>
2019-01-14 23:02:08 +0800

    aaa

---
+++ src/main/java/com/zhaoyi/crawler/bean/AiqiYiDetailPageInfo.java
@@ -0,0 +1,318 @@
+/**
+  * Copyright 2019 bejson.com 
+  */
+package com.zhaoyi.crawler.bean;
+import java.util.List;
+import java.util.Date;
+
+/**
+ * Auto-generated: 2019-01-14 22:13:52
+ *
+ * @author bejson.com (i@bejson.com)
+ * @website http://www.bejson.com/java2pojo/
+ */
+public class AiqiYiDetailPageInfo {
+    @Override
+    public String toString() {
+        return "AiQiYiOldMovie{" +
+                "albumId=" + albumId +
+                ", albumName='" + albumName + '\'' +
+                ", imageUrl='" + imageUrl + '\'' +
+                ", tvId=" + tvId +
+                ", vid='" + vid + '\'' +
+                ", cid=" + cid +
+                ", isSource=" + isSource +
+                ", contentType=" + contentType +
+                ", vType='" + vType + '\'' +
+                ", pType='" + pType + '\'' +
+                ", pageNo='" + pageNo + '\'' +
+                ", pageType='" + pageType + '\'' +
+                ", userId=" + userId +
+                ", pageUrl='" + pageUrl + '\'' +
+                ", tvName='" + tvName + '\'' +
+                ", isfeizhengpian='" + isfeizhengpian + '\'' +
+                ", categoryName='" + categoryName + '\'' +
+                ", categories='" + categories + '\'' +
+                ", downloadAllowed=" + downloadAllowed +
+                ", publicLevel='" + publicLevel + '\'' +
+                ", payMark=" + payMark +
+                ", payMarkUrl='" + payMarkUrl + '\'' +
+                ", vipType=" + vipType +
+                ", qiyiProduced=" + qiyiProduced +
+                ", exclusive=" + exclusive +
+                ", tvYear='" + tvYear + '\'' +
+                ", duration='" + duration + '\'' +
+                ", wallId=" + wallId +
+                ", rewardAllowed=" + rewardAllowed +
+                ", commentAllowed=" + commentAllowed +
+                ", heatShowTypes='" + heatShowTypes + '\'' +
+                ", videoTemplate=" + videoTemplate +
+                ", issueTime=" + issueTime +
+                '}';
+    }
+
+    private int albumId;
+    private String albumName;
+    private String imageUrl;
+    private long tvId;
+    private String vid;
+    private int cid;
+    private boolean isSource;
+    private int contentType;
+    private String vType;
+    private String pType;
+    private String pageNo;
+    private String pageType;
+    private int userId;
+    private String pageUrl;
+    private String tvName;
+    private String isfeizhengpian;
+    private String categoryName;
+    private String categories;
+    private int downloadAllowed;
+    private String publicLevel;
+    private int payMark;
+    private String payMarkUrl;
+    private List<Integer> vipType;
+    private int qiyiProduced;
+    private int exclusive;
+    private String tvYear;
+    private String duration;
+    private int wallId;
+    private int rewardAllowed;
+    private int commentAllowed;
+    private String heatShowTypes;
+    private int videoTemplate;
+    private Date issueTime;
+    public void setAlbumId(int albumId) {
+         this.albumId = albumId;
+     }
+     public int getAlbumId() {
+         return albumId;
+     }
+
+    public void setAlbumName(String albumName) {
+         this.albumName = albumName;
+     }
+     public String getAlbumName() {
+         return albumName;
+     }
+
+    public void setImageUrl(String imageUrl) {
+         this.imageUrl = imageUrl;
+     }
+     public String getImageUrl() {
+         return imageUrl;
+     }
+
+    public void setTvId(long tvId) {
+         this.tvId = tvId;
+     }
+     public long getTvId() {
+         return tvId;
+     }
+
+    public void setVid(String vid) {
+         this.vid = vid;
+     }
+     public String getVid() {
+         return vid;
+     }
+
+    public void setCid(int cid) {
+         this.cid = cid;
+     }
+     public int getCid() {
+         return cid;
+     }
+
+    public void setIsSource(boolean isSource) {
+         this.isSource = isSource;
+     }
+     public boolean getIsSource() {
+         return isSource;
+     }
+
+    public void setContentType(int contentType) {
+         this.contentType = contentType;
+     }
+     public int getContentType() {
+         return contentType;
+     }
+
+    public void setVType(String vType) {
+         this.vType = vType;
+     }
+     public String getVType() {
+         return vType;
+     }
+
+    public void setPType(String pType) {
+         this.pType = pType;
+     }
+     public String getPType() {
+         return pType;
+     }
+
+    public void setPageNo(String pageNo) {
+         this.pageNo = pageNo;
+     }
+     public String getPageNo() {
+         return pageNo;
+     }
+
+    public void setPageType(String pageType) {
+         this.pageType = pageType;
+     }
+     public String getPageType() {
+         return pageType;
+     }
+
+    public void setUserId(int userId) {
+         this.userId = userId;
+     }
+     public int getUserId() {
+         return userId;
+     }
+
+    public void setPageUrl(String pageUrl) {
+         this.pageUrl = pageUrl;
+     }
+     public String getPageUrl() {
+         return pageUrl;
+     }
+
+    public void setTvName(String tvName) {
+         this.tvName = tvName;
+     }
+     public String getTvName() {
+         return tvName;
+     }
+
+    public void setIsfeizhengpian(String isfeizhengpian) {
+         this.isfeizhengpian = isfeizhengpian;
+     }
+     public String getIsfeizhengpian() {
+         return isfeizhengpian;
+     }
+
+    public void setCategoryName(String categoryName) {
+         this.categoryName = categoryName;
+     }
+     public String getCategoryName() {
+         return categoryName;
+     }
+
+    public void setCategories(String categories) {
+         this.categories = categories;
+     }
+     public String getCategories() {
+         return categories;
+     }
+
+    public void setDownloadAllowed(int downloadAllowed) {
+         this.downloadAllowed = downloadAllowed;
+     }
+     public int getDownloadAllowed() {
+         return downloadAllowed;
+     }
+
+    public void setPublicLevel(String publicLevel) {
+         this.publicLevel = publicLevel;
+     }
+     public String getPublicLevel() {
+         return publicLevel;
+     }
+
+    public void setPayMark(int payMark) {
+         this.payMark = payMark;
+     }
+     public int getPayMark() {
+         return payMark;
+     }
+
+    public void setPayMarkUrl(String payMarkUrl) {
+         this.payMarkUrl = payMarkUrl;
+     }
+     public String getPayMarkUrl() {
+         return payMarkUrl;
+     }
+
+    public void setVipType(List<Integer> vipType) {
+         this.vipType = vipType;
+     }
+     public List<Integer> getVipType() {
+         return vipType;
+     }
+
+    public void setQiyiProduced(int qiyiProduced) {
+         this.qiyiProduced = qiyiProduced;
+     }
+     public int getQiyiProduced() {
+         return qiyiProduced;
+     }
+
+    public void setExclusive(int exclusive) {
+         this.exclusive = exclusive;
+     }
+     public int getExclusive() {
+         return exclusive;
+     }
+
+    public void setTvYear(String tvYear) {
+         this.tvYear = tvYear;
+     }
+     public String getTvYear() {
+         return tvYear;
+     }
+
+    public void setDuration(String duration) {
+         this.duration = duration;
+     }
+     public String getDuration() {
+         return duration;
+     }
+
+    public void setWallId(int wallId) {
+         this.wallId = wallId;
+     }
+     public int getWallId() {
+         return wallId;
+     }
+
+    public void setRewardAllowed(int rewardAllowed) {
+         this.rewardAllowed = rewardAllowed;
+     }
+     public int getRewardAllowed() {
+         return rewardAllowed;
+     }
+
+    public void setCommentAllowed(int commentAllowed) {
+         this.commentAllowed = commentAllowed;
+     }
+     public int getCommentAllowed() {
+         return commentAllowed;
+     }
+
+    public void setHeatShowTypes(String heatShowTypes) {
+         this.heatShowTypes = heatShowTypes;
+     }
+     public String getHeatShowTypes() {
+         return heatShowTypes;
+     }
+
+    public void setVideoTemplate(int videoTemplate) {
+         this.videoTemplate = videoTemplate;
+     }
+     public int getVideoTemplate() {
+         return videoTemplate;
+     }
+
+    public void setIssueTime(Date issueTime) {
+         this.issueTime = issueTime;
+     }
+     public Date getIssueTime() {
+         return issueTime;
+     }
+
+}
--- src/main/java/com/zhaoyi/crawler/bean/Movie.java
+++ src/main/java/com/zhaoyi/crawler/bean/Movie.java
@@ -1,10 +1,87 @@
 package com.zhaoyi.crawler.bean;
 
+import java.util.Date;
+
 public class Movie {
     private String name;
     private String url;
-    private MovieType movieType;
+    private String movieType;
+    private String coverImg;
+    private String roleInfo;
+    private String description;
+    private Date issueTime;
+    private String categories;
+    private String duration;
 
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
 
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getMovieType() {
+        return movieType;
+    }
+
+    public void setMovieType(String movieType) {
+        this.movieType = movieType;
+    }
+
+    public void setCoverImg(String coverImg) {
+        this.coverImg = coverImg;
+    }
+
+    public String getCoverImg() {
+        return coverImg;
+    }
+
+    public void setRoleInfo(String roleInfo) {
+        this.roleInfo = roleInfo;
+    }
+
+    public String getRoleInfo() {
+        return roleInfo;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setIssueTime(Date issueTime) {
+        this.issueTime = issueTime;
+    }
+
+    public Date getIssueTime() {
+        return issueTime;
+    }
+
+    public void setCategories(String categories) {
+        this.categories = categories;
+    }
+
+    public String getCategories() {
+        return categories;
+    }
+
+    public void setDuration(String duration) {
+        this.duration = duration;
+    }
+
+    public String getDuration() {
+        return duration;
+    }
 }
